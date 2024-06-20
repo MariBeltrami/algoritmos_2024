01d20f236367227cb88066001c59d12b103fdd95
Mari Beltrami <mariabelt13@gmail.com>
2024-06-19 23:57:55 -0300

    commit

--- 4. Lista/Ej6.py
+++ 4. Lista/Ej6.py
@@ -13,152 +13,69 @@ g. mostrar toda la información de Flash y Star-Lord;
 h. listar los superhéroes que comienzan con la letra B, M y S;
 i. determinar cuántos superhéroes hay de cada casa de comic."""
 
-from lista import remove
+from lista import remove, search, show_list
+from lista_superheroes import super_heroes
 
-super_heroes = [
-  {
-    "nombre": "Linterna Verde",
-    "año_aparicion": 1940,
-    "casa_comic": "DC Comics",
-    "biografia": "Miembro de la Tropa de Linternas Verdes, posee un anillo que le otorga poderes basados en la fuerza de voluntad."
-  },
-  {
-    "nombre": "Wolverine",
-    "año_aparicion": 1974,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Mutante con garras retráctiles y habilidades regenerativas, miembro de los X-Men."
-  },
-  {
-    "nombre": "Doctor Strange",
-    "año_aparicion": 1963,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Hechicero supremo del universo Marvel, maestro de las artes místicas y protector de la realidad."
-  },
-  {
-    "nombre": "Capitana Marvel",
-    "año_aparicion": 1968,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Heroína cósmica con poderes de vuelo, fuerza sobrehumana y energía cósmica."
-  },
-  {
-    "nombre": "Mujer Maravilla",
-    "año_aparicion": 1941,
-    "casa_comic": "DC Comics",
-    "biografia": "Princesa amazona y una de las principales defensoras de la justicia y la igualdad en el Universo DC."
-  },
-  {
-    "nombre": "Flash",
-    "año_aparicion": 1940,
-    "casa_comic": "DC Comics",
-    "biografia": "Velocista con la capacidad de correr a velocidades superiores a la luz, miembro de la Liga de la Justicia."
-  },
-  {
-    "nombre": "Star-Lord",
-    "año_aparicion": 1976,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Líder de los Guardianes de la Galaxia, experto en combate y estrategia intergaláctica."
-  },
-  {
-    "nombre": "Superman",
-    "año_aparicion": 1938,
-    "casa_comic": "DC Comics",
-    "biografia": "El Hombre de Acero, uno de los héroes más icónicos de DC con superpoderes sobrehumanos."
-  },
-  {
-    "nombre": "Batman",
-    "año_aparicion": 1939,
-    "casa_comic": "DC Comics",
-    "biografia": "El Caballero Oscuro, detective y luchador experto que protege Gotham City."
-  },
-  {
-    "nombre": "Iron Man",
-    "año_aparicion": 1963,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Tony Stark, genio multimillonario y superhéroe con una armadura tecnológica de alta tecnología."
-  },
-  {
-    "nombre": "Wonder Woman",
-    "año_aparicion": 1941,
-    "casa_comic": "DC Comics",
-    "biografia": "La princesa amazona Diana, guerrera y defensora de la paz y la justicia en el mundo."
-  },
-  {
-    "nombre": "Spider-Man",
-    "año_aparicion": 1962,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Peter Parker, joven héroe con habilidades arácnidas tras ser picado por una araña radiactiva."
-  },
-  {
-    "nombre": "Thor",
-    "año_aparicion": 1962,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Dios nórdico del trueno y miembro de los Vengadores, posee un martillo encantado llamado Mjolnir."
-  },
-  {
-    "nombre": "Aquaman",
-    "año_aparicion": 1941,
-    "casa_comic": "DC Comics",
-    "biografia": "Rey de Atlantis con la capacidad de comunicarse con la vida marina y controlar el agua."
-  },
-  {
-    "nombre": "Green Arrow",
-    "año_aparicion": 1941,
-    "casa_comic": "DC Comics",
-    "biografia": "Oliver Queen, arquero habilidoso y defensor de la justicia con su arco y flechas."
-  },
-  {
-    "nombre": "Hulk",
-    "año_aparicion": 1962,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Bruce Banner, científico transformado en monstruo verde con fuerza increíble."
-  },
-  {
-    "nombre": "Black Widow",
-    "año_aparicion": 1964,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Natasha Romanoff, espía rusa y experta en combate mano a mano y armas."
-  },
-  {
-    "nombre": "Mr. Fantástico",
-    "año_aparicion": 1961,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Líder de los 4 Fantásticos, científico brillante con la capacidad de estirar y deformar su cuerpo."
-  },
-  {
-    "nombre": "La Mujer Invisible",
-    "año_aparicion": 1961,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Miembro de los 4 Fantásticos, posee el poder de hacerse invisible y crear campos de fuerza."
-  },
-  {
-    "nombre": "La Antorcha Humana",
-    "año_aparicion": 1961,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Miembro de los 4 Fantásticos, puede envolverse en llamas y volar a altas velocidades."
-  },
-  {
-    "nombre": "La Cosa",
-    "año_aparicion": 1961,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Miembro de los 4 Fantásticos, posee una fuerza y resistencia sobrehumanas, con piel rocosa."
-  },
-  {
-    "nombre": "Capitán América",
-    "año_aparicion": 1941,
-    "casa_comic": "Marvel Comics",
-    "biografia": "El supersoldado Steve Rogers, símbolo de libertad y justicia con escudo indestructible."
-  },
-  {
-    "nombre": "Ant-Man",
-    "año_aparicion": 1962,
-    "casa_comic": "Marvel Comics",
-    "biografia": "Hanbiografiak Pym o Scott Lang, héroes capaces de cambiar de tamaño y comunicarse con insectos. Usa un traje que lo hace pequeño."
-  }
-]
+def eliminar_superheroe(lista, nombre):
+    remove(lista, 'nombre', nombre)
 
-"""a. eliminar el nodo que contiene la información de Linterna Verde;"""
+def mostrar_anio_aparicion(lista, nombre):
+    index = search(lista, 'nombre', nombre)
+    if index is not None:
+        print(f"Año de aparición de {nombre}: {lista[index]['año_aparicion']}")
 
-criterio= "Linterna verde"
-eliminar = 'Linterna verde'
-result_delete = remove(super_heroes, criterio, eliminar)
-print(f'Eliminar {eliminar} resultado: {result_delete}')
+def cambiar_casa(lista, nombre, nueva_casa):
+    index = search(lista, 'nombre', nombre)
+    if index is not None:
+        lista[index]['casa'] = nueva_casa
+
+def mostrar_superheroes_con_palabras(lista, palabras):
+    for super_heroes in lista:
+        if any(palabra in super_heroes['biografia'] for palabra in palabras):
+            print(f"Superhéroe con {' o '.join(palabras)} en su biografía: {super_heroes['nombre']}")
+
+def mostrar_heroes_anteriores_a(lista, anio):
+    for super_heroes in lista:
+        if super_heroes['año_aparicion'] < anio:
+            print(f"Superhéroe anterior a {anio}: {super_heroes['nombre']}, Casa: {super_heroes['casa_comic']}")
+
+def mostrar_casa_heroes(lista, nombres):
+    for nombre in nombres:
+        index = search(lista, 'nombre', nombre)
+        if index is not None:
+            print(f"Casa de {nombre}: {lista[index]['casa_comic']}")
+
+def mostrar_info_heroes(lista, nombres):
+    for nombre in nombres:
+        index = search(lista, 'nombre', nombre)
+        if index is not None:
+            info = lista[index]
+            print(f"Información de {nombre}: Nombre: {info['nombre']}, Año: {info['año_aparicion']}, Casa: {info['casa_comic']}, Biografía: {info['biografia']}")
+
+def listar_heroes_por_letra(lista, letras):
+    for superheroe in lista:
+        if superheroe['nombre'][0] in letras:
+            print(f"Superhéroe que comienza con {', '.join(letras)}: {superheroe['nombre']}")
+
+def contar_heroes_por_casa(lista):
+    marvel_count = 0
+    dc_count = 0
+    for super_heroes in lista:
+        if super_heroes['casa_comic'] == "Marvel":
+            marvel_count += 1
+        elif super_heroes['casa_comic'] == "DC":
+            dc_count += 1
+    print(f"Cantidad de superhéroes de Marvel: {marvel_count}")
+    print(f"Cantidad de superhéroes de DC: {dc_count}")
+
+eliminar_superheroe(super_heroes, "Linterna Verde")
+mostrar_anio_aparicion(super_heroes, "Wolverine")
+cambiar_casa(super_heroes, "Dr. Strange", "Marvel")
+mostrar_superheroes_con_palabras(super_heroes, ["traje", "armadura"])
+mostrar_heroes_anteriores_a(super_heroes, 1963)
+mostrar_casa_heroes(super_heroes, ["Capitana Marvel", "Mujer Maravilla"])
+mostrar_info_heroes(super_heroes, ["Flash", "Star-Lord"])
+listar_heroes_por_letra(super_heroes, ["B", "M", "S"])
+contar_heroes_por_casa(super_heroes)
+
+show_list("Lista de Superhéroes", super_heroes)
---
+++ 4. Lista/pokemones.py
@@ -0,0 +1,73 @@
+pokemons = [
+    {
+        "nombre": "Pikachu",
+        "nivel": 35,
+        "tipo": "Eléctrico",
+        "subtipo": ""
+    },
+    {
+        "nombre": "Charizard",
+        "nivel": 40,
+        "tipo": "Fuego",
+        "subtipo": "Volador"
+    },
+    {
+        "nombre": "Bulbasaur",
+        "nivel": 30,
+        "tipo": "Planta",
+        "subtipo": "Veneno"
+    },
+    {
+        "nombre": "Starmie",
+        "nivel": 30,
+        "tipo": "Agua",
+        "subtipo": "Psíquico"
+    },
+    {
+        "nombre": "Psyduck",
+        "nivel": 25,
+        "tipo": "Agua",
+        "subtipo": ""
+    },
+    {
+        "nombre": "Gyarados",
+        "nivel": 35,
+        "tipo": "Agua",
+        "subtipo": "Volador"
+    },
+    {
+        "nombre": "Onix",
+        "nivel": 38,
+        "tipo": "Roca",
+        "subtipo": "Tierra"
+    },
+    {
+        "nombre": "Geodude",
+        "nivel": 28,
+        "tipo": "Roca",
+        "subtipo": "Tierra"
+    },
+    {
+        "nombre": "Vulpix",
+        "nivel": 20,
+        "tipo": "Fuego",
+        "subtipo": ""
+    },
+    {
+        "nombre": "Blastoise",
+        "nivel": 50,
+        "tipo": "Agua",
+        "subtipo": ""
+    },
+    {
+        "nombre": "Umbreon",
+        "nivel": 45,
+        "tipo": "Siniestro",
+        "subtipo": ""
+    },
+    {
+        "nombre": "Nidoking",
+        "nivel": 40,
+        "tipo": "Veneno",
+        "subtipo": "Tierra"
+    }]
